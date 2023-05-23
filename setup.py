28c06239009bf317620b71251318f8dfc08a6b84
mxRecTeam <mxRecTeam>
2023-05-23 11:05:40 +0800

    Match-id-aff4f959a1755e6d80f18ac80a7629b3ed62d239

--- setup.py
+++ setup.py
@@ -7,7 +7,25 @@
 # History: NA
 
 import os
+import stat
 from setuptools import setup, find_packages
+import pkg_resources
+from setuptools.extern.packaging import version as packaging_version
+
+
+# Patch Version class to preserve original version string
+class NoNormalizeVersion(packaging_version.Version):
+    def __init__(self, version):
+        self._orig_version = version
+        super().__init__(version)
+
+    def __str__(self):
+        return self._orig_version
+
+
+packaging_version.Version = NoNormalizeVersion
+# Patch safe_version() to prevent version normalization
+pkg_resources.safe_version = lambda v: v
 
 try:
     with open("README.md") as file:
@@ -17,6 +35,21 @@ except IOError:
 
 env_version = os.getenv("VERSION")
 VERSION = env_version if env_version is not None else '5.0.T104'
+
+INIT_FILE = "mx_rec/__init__.py"
+with open(INIT_FILE, 'r') as file:
+    lines = file.readlines()
+
+for idx, line in enumerate(lines):
+    if "__version__ = " not in line:
+        continue
+    lines[idx] = f"__version__ = '{VERSION}'\n"
+    break
+
+FLAG = os.O_WRONLY | os.O_TRUNC
+MODE = stat.S_IWUSR | stat.S_IRUSR | stat.S_IRGRP | stat.S_IROTH
+with os.fdopen(os.open(INIT_FILE, FLAG, MODE), 'w') as out:
+    out.writelines(lines)
 
 setup(
     name='mx_rec',
