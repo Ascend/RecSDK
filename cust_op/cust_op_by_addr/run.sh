b2b0b269b7d1cebba1638ccb5c12079a50f5c37c
mxRecTeam <mxRecTeam>
2023-06-14 10:25:53 +0800

    Match-id-3e98bc6597cebe13d2babdfb2d67bbe1be23daf2

--- cust_op/cust_op_by_addr/run.sh
+++ cust_op/cust_op_by_addr/run.sh
@@ -1,12 +1,18 @@
-
-#source /usr/local/Ascend/ascend-toolkit/set_env.sh
+#!/bin/bash
+# Copyright (c) Huawei Technologies Co., Ltd. 2023-2023. All rights reserved.
+# Description: Build for cust_op by address
+# Author: MindX SDK
+# Create: 2023
+# History: NA
+set -e
 source /etc/profile
-msopgen_path=$(find /usr/local/Ascend/ -name msopgen | grep bin)
-# 截取上一层目录
-parent_dir=$(dirname "$msopgen_path")
 
+# 查找msopgen的路径，加入到环境变量PATH中
+msopgen_path=$(find /usr/local/Ascend/ -name msopgen | grep bin)
+parent_dir=$(dirname "$msopgen_path")
 export PATH=$parent_dir:$PATH
 
+# 利用msopgen生成可编译文件
 rm -rf ./custom_op
 msopgen gen -i emb_custom.json -f tf -c ai_core-ascend910b -lan cpp -out ./custom_op -m 0 -op EmbeddingLookupByAddress
 msopgen gen -i emb_custom.json -f tf -c ai_core-ascend910b -lan cpp -out ./custom_op -m 1 -op EmbeddingUpdateByAddress
@@ -16,15 +22,13 @@ cp -rf op_host custom_op/
 
 cd custom_op
 
-# 判断当前目录下是否存在cmake.json文件
+# 判断当前目录下是否存在CMakePresets.json文件
 if [ ! -f "CMakePresets.json" ]; then
   echo "当前目录下不存在cmake.json文件"
   exit 1
 fi
 
-#jq '.configurePresets.cacheVariables.ASCEND_CANN_PACKAGE_PATH.value = "/usr/local/Ascend/ascend-toolkit/latest"' CMakePresets.json > tmp.json
-#mv tmp.json cmake.json
-
+# 修改cann安装路径
 sed -i 's:"/usr/local/Ascend/latest":"/usr/local/Ascend/ascend-toolkit/latest":g' CMakePresets.json
 
 cd cmake
@@ -35,7 +39,7 @@ if [ ! -f "config.cmake" ]; then
   exit 1
 fi
 
-
+# 修改设备环境
 sed -i 's:set(ASCEND_COMPUTE_UNIT ascend910b):set(ASCEND_COMPUTE_UNIT ascend910b ascend910):g' config.cmake
 
 cd ..
