29786fe1ba39d8e46963b4af32588c13fe9e5592
mxRecTeam <mxRecTeam>
2023-06-25 16:04:09 +0800

    Match-id-6e23104be69b03fdb7dc0e38ade4a7bcdb095ae1

--- src/core/checkpoint/checkpoint.h
+++ src/core/checkpoint/checkpoint.h
@@ -31,7 +31,7 @@ namespace MxRec {
         const string dataFileType { ".data" };
         const string attribFileType { ".attribute" };
         const string dirSeparator { "/" };
-        const mode_t dirMode { 0777 };
+        const mode_t dirMode { 0755 };
 
         const string currDir { "." };
         const string prevDir { ".." };
