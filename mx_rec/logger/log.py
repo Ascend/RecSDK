ebe96229380c54df05ff1d22dec71a0639827b62
mxRecTeam <mxRecTeam>
2023-11-04 11:56:58 +0800

    Match-id-48b1b808025e3a5d130cb594c7610f83baa26f58

--- mx_rec/logger/log.py
+++ mx_rec/logger/log.py
@@ -14,19 +14,16 @@ from mx_rec.util.global_env_conf import global_env
 def init_sys_log():
     work_dir = os.path.dirname(os.path.dirname(__file__))
     log_cfg_file = os.path.join(work_dir, "logger.yaml")
-    real_config_path = os.path.realpath(log_cfg_file)
-
-    if not FileValidator("log_cfg_file", log_cfg_file).check_file_size(real_config_path).check().is_valid():
-        raise ValueError("Config file size is not valid.")
-
-    with open(real_config_path, 'r', encoding='utf-8') as open_file:
-        if not FileValidator("log_cfg_file", real_config_path). \
+    with open(log_cfg_file, 'r', encoding='utf-8'):
+        if not FileValidator("log_cfg_file", log_cfg_file). \
                 check_file_size(LOG_MAX_SIZE). \
                 check_not_soft_link(). \
                 check_user_group(). \
                 is_valid():
             raise ValueError("Log config file is not valid.")
 
+    real_config_path = os.path.realpath(log_cfg_file)
+    with open(real_config_path, 'r', encoding='utf-8') as open_file:
         data = open_file.read(LOG_MAX_SIZE)
         log_cfg = yaml.safe_load(data)
 
