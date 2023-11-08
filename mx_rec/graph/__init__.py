2bea3d7b61fcd960619cf28e3eeba6e29d6dad35
mxRecTeam <mxRecTeam>
2023-11-08 17:22:23 +0800

    Match-id-259ff22e78c7ad37e6dd0dba7a3ac9c747a66299

--- mx_rec/graph/__init__.py
+++ mx_rec/graph/__init__.py
@@ -2,4 +2,7 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) Huawei Technologies Co., Ltd. 2022-2023. All rights reserved.
 
-__all__ = ["modifier"]
+__all__ = ["modify_graph_and_start_emb_cache", "GraphModifierHook", "run"]
+
+from mx_rec.graph.modifier import GraphModifierHook, modify_graph_and_start_emb_cache
+from mx_rec.graph.patch import run
--- mx_rec/util/__init__.py
+++ mx_rec/util/__init__.py
@@ -2,4 +2,15 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) Huawei Technologies Co., Ltd. 2022-2023. All rights reserved.
 
-__all__ = ["initialize", "variable"]
+__all__ = [
+    "init", "get_rank_id", "get_initializer", "terminate_config_initializer", "clear_channel",
+    "get_dense_and_sparse_variable", "set_if_load", "set_ascend_global_hashtable_collection",
+    "get_ascend_global_hashtable_collection", "get_rank_size", "get_host_pipeline_ops",
+    "get_use_dynamic_expansion", "set_ascend_table_name_must_contain", "get_target_batch"
+]
+
+from mx_rec.util.tf_version_adapter import npu_ops, hccl_ops, NPUCheckpointSaverHook
+from mx_rec.util.initialize import init, get_rank_id, get_initializer, terminate_config_initializer, clear_channel, \
+    set_if_load, set_ascend_global_hashtable_collection, get_ascend_global_hashtable_collection, get_rank_size, \
+    get_host_pipeline_ops, get_use_dynamic_expansion, set_ascend_table_name_must_contain, get_target_batch
+from mx_rec.util.variable import get_dense_and_sparse_variable
