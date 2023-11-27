5ea44a49ae8d2480d8276aa90f45f485864598f4
mxRecTeam <mxRecTeam>
2023-11-27 19:35:27 +0800

    Match-id-79d71a725755c5074827f95c847a8a3b1e06c68a

--- cust_op/cust_op_by_addr/op_host/embedding_update_by_address.cpp
+++ cust_op/cust_op_by_addr/op_host/embedding_update_by_address.cpp
@@ -4,6 +4,14 @@
 
 namespace optiling
 {
+    constexpr int32_t BLOCK_DIM = 48;  // 910b一张卡48个vector核
+    constexpr int32_t SIZE_OF_HALF = 2;
+    constexpr int32_t SIZE_OF_FLOAT_OR_INT = 4;
+    constexpr int32_t MIN_BLOCK_SIZE = 32; // ub空间的数据都要按照32对齐
+    constexpr int32_t UB_LIMIT = 175 * 1024;
+    constexpr int32_t USR_SIZE = 256;
+    constexpr int32_t SYS_WORKSPACE_SIZE = 16 * 1024 * 1024;
+    constexpr int32_t PING_PONG_NUM = 1;
 
     template<typename T>
     static ge::graphStatus CheckPointer(T *pointer, const char *errorMessage)
@@ -28,32 +36,30 @@ namespace optiling
 
     static ge::graphStatus TilingFunc(gert::TilingContext *context)
     {
-        TilingData2 tiling;
-
-        size_t usrSize = 256, sysWorkspaceSize = 16 * 1024 * 1024;
-        if (CheckPointer(context, "Update embbedingType context") != ge::GRAPH_SUCCESS)
+        if (CheckPointer(context, "Update embeddingType context") != ge::GRAPH_SUCCESS) {
             return ge::GRAPH_FAILED;
+        }
 
         size_t *currentWorkspace = context->GetWorkspaceSizes(1);
-        if (CheckPointer(currentWorkspace, "currentWorkspace") != ge::GRAPH_SUCCESS)
+        if (CheckPointer(currentWorkspace, "currentWorkspace") != ge::GRAPH_SUCCESS) {
             return ge::GRAPH_FAILED;
-
-        currentWorkspace[0] = sysWorkspaceSize + usrSize;
-
-        int32_t blockTotalNums = 48;
-        int32_t ubLimit = 175 * 1024;
+        }
+        currentWorkspace[0] = SYS_WORKSPACE_SIZE + USR_SIZE;
 
         auto inputTensor = context->GetInputTensor(0);
-        if (CheckPointer(inputTensor, "GetInputTensor inputTensor") != ge::GRAPH_SUCCESS)
+        if (CheckPointer(inputTensor, "GetInputTensor inputTensor") != ge::GRAPH_SUCCESS) {
             return ge::GRAPH_FAILED;
+        }
 
         int32_t inputShape = inputTensor->GetShapeSize();
-        if (CheckPositiveInt(inputShape, "inputShape") != ge::GRAPH_SUCCESS)
+        if (CheckPositiveInt(inputShape, "inputShape") != ge::GRAPH_SUCCESS) {
             return ge::GRAPH_FAILED;
+        }
 
         auto inputTensor1 = context->GetInputTensor(1);
-        if (CheckPointer(inputTensor1, "GetInputTensor inputTensor1") != ge::GRAPH_SUCCESS)
+        if (CheckPointer(inputTensor1, "GetInputTensor inputTensor1") != ge::GRAPH_SUCCESS) {
             return ge::GRAPH_FAILED;
+        }
 
         int32_t inputDim = inputTensor1->GetShapeSize() / inputShape;
         if (CheckPositiveInt(inputDim, "inputDim") != ge::GRAPH_SUCCESS) {
@@ -61,56 +67,54 @@ namespace optiling
         }
 
         auto attrs = context->GetAttrs();
-        if (CheckPointer(attrs, "GetAttrs attrs") != ge::GRAPH_SUCCESS)
+        if (CheckPointer(attrs, "GetAttrs attrs") != ge::GRAPH_SUCCESS) {
             return ge::GRAPH_FAILED;
+        }
 
         auto attrPointer = attrs->GetAttrPointer<int64_t>(0);
-        if (CheckPointer(attrPointer, "attrPointer") != ge::GRAPH_SUCCESS)
+        if (CheckPointer(attrPointer, "attrPointer") != ge::GRAPH_SUCCESS) {
             return ge::GRAPH_FAILED;
+        }
 
         int32_t updateType = *(attrPointer);
         ge::DataType inputDatatype = inputTensor1->GetDataType();
-        int32_t embbedingType;
+        int32_t embeddingType;
         if (inputDatatype == ge::DT_FLOAT16) {
-            embbedingType = 2;
+            embeddingType = 2;
         } else if (inputDatatype == ge::DT_INT32) {
-            embbedingType = 0;
+            embeddingType = 0;
         } else {
-            embbedingType = 1;
+            embeddingType = 1;
         }
 
-        int32_t singleDataSize = 4;
-        if (embbedingType == 2) {
-            singleDataSize = 2;
+        int32_t typeSize = SIZE_OF_FLOAT_OR_INT;
+        if (embeddingType == 2) {
+            typeSize = SIZE_OF_HALF;
         }
-        int32_t minMoveNum = 32 / singleDataSize;
+        int32_t alignNum = MIN_BLOCK_SIZE / typeSize;
 
-        // onceMoveNums，(updateDim - 1 + minMoveNum) / min_move_num表示除以min_move_num向下取整
-        int32_t onceMoveNums = minMoveNum * ((inputDim - 1 + minMoveNum) / minMoveNum);
+        int32_t inputDimAligned = alignNum * ((inputDim - 1 + alignNum) / alignNum);
 
-        int32_t numToMove = (inputDim - 1 + onceMoveNums) / onceMoveNums;
         // 每个地址需要占用sizeof(int64_t)个字节，singleDataSize表示每个数据的字节数，需要使用2倍的内存空间，因为每次移动都需要复制一份数据
-        int32_t pingPongNum = 1;
         int32_t occupyAddressBytesNum =
-                sizeof(int64_t) + singleDataSize * onceMoveNums * numToMove * pingPongNum * 2;
-        // 计算一轮计算中最多计算多少个addr，最后的 /4 再*4 是为了与32对齐，因为sizeof(int64_t) = 8
-        int32_t addrMaxNum = ((int)((int)(ubLimit / occupyAddressBytesNum) / 4)) * 4;
-        if (CheckPositiveInt(addrMaxNum, "addrMaxNum") != ge::GRAPH_SUCCESS) {
+                sizeof(int64_t) + typeSize * inputDimAligned * PING_PONG_NUM * 2;
+        // 一轮计算中最多计算多少个addr，由于地址也要搬到ub，所以需要对齐32
+        int32_t addrPerLoop = (UB_LIMIT / occupyAddressBytesNum) & (~3); // & (~3)，保证地址数是4的倍数
+        if (CheckPositiveInt(addrPerLoop, "addrPerLoop") != ge::GRAPH_SUCCESS) {
             return ge::GRAPH_FAILED;
         }
+        TilingData2 tiling;
+        tiling.set_ping_pong_num(PING_PONG_NUM);
 
         tiling.set_update_type(updateType);
-        tiling.set_embbeding_type(embbedingType);
+        tiling.set_embedding_type(embeddingType);
         tiling.set_update_dim(inputDim);
         tiling.set_addr_nums(inputShape);
-        tiling.set_ub_limit(ubLimit);
+        tiling.set_addr_per_loop(addrPerLoop);
+        tiling.set_type_size(typeSize);
+        tiling.set_input_dim_aligned(inputDimAligned);
 
-        tiling.set_addr_max_num(addrMaxNum);
-        tiling.set_ping_pong_num(pingPongNum);
-        tiling.set_single_data_size(singleDataSize);
-        tiling.set_once_move_nums(onceMoveNums);
-
-        context->SetBlockDim(blockTotalNums);
+        context->SetBlockDim(BLOCK_DIM);
         tiling.SaveToBuffer(context->GetRawTilingData()->GetData(), context->GetRawTilingData()->GetCapacity());
         context->GetRawTilingData()->SetDataSize(tiling.GetDataSize());
 
--- cust_op/cust_op_by_addr/op_host/embedding_update_by_address_tiling.h
+++ cust_op/cust_op_by_addr/op_host/embedding_update_by_address_tiling.h
@@ -1,4 +1,5 @@
-
+#ifndef EMBEDDING_UPDATE_BY_ADDRESS_TILING_H
+#define EMBEDDING_UPDATE_BY_ADDRESS_TILING_H
 #include "register/tilingdata_base.h"
 
 namespace optiling
@@ -6,15 +7,15 @@ namespace optiling
 BEGIN_TILING_DATA_DEF(TilingData2)
     TILING_DATA_FIELD_DEF(int32_t, update_dim);
     TILING_DATA_FIELD_DEF(int32_t, addr_nums);
-    TILING_DATA_FIELD_DEF(int32_t, ub_limit);
-    TILING_DATA_FIELD_DEF(int32_t, embbeding_type);
+    TILING_DATA_FIELD_DEF(int32_t, embedding_type);
     TILING_DATA_FIELD_DEF(int32_t, update_type);
-    TILING_DATA_FIELD_DEF(int32_t, addr_max_num);
+    TILING_DATA_FIELD_DEF(int32_t, addr_per_loop);
     TILING_DATA_FIELD_DEF(int32_t, ping_pong_num);
-    TILING_DATA_FIELD_DEF(int32_t, single_data_size);
-    TILING_DATA_FIELD_DEF(int32_t, once_move_nums);
+    TILING_DATA_FIELD_DEF(int32_t, type_size);
+    TILING_DATA_FIELD_DEF(int32_t, input_dim_aligned);
 
 END_TILING_DATA_DEF;
 
     REGISTER_TILING_DATA_CLASS(EmbeddingUpdateByAddress, TilingData2)
 }
+#endif // EMBEDDING_UPDATE_BY_ADDRESS_TILING_H
