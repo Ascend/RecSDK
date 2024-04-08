b85136384f035fcea249b4452d9a119cc633c253
何霖 <helin_1@163.com>
2024-04-08 19:26:38 +0800

    mxrec构建优化：使用python3.7 setup.py bdist_wheel方式构建

--- setup.py
+++ setup.py
@@ -16,64 +16,25 @@
 # ==============================================================================
 
 import os
-import stat
-from setuptools import setup, find_packages
-import pkg_resources
-from setuptools.extern.packaging import version as packaging_version
+import shutil
+import subprocess
 
+# clean pkg_dir existed
+pkg_dir = "./build/mindxsdk-mxrec"
+if os.path.exists(pkg_dir):
+    shutil.rmtree(pkg_dir)
 
-# Patch Version class to preserve original version string
-class NoNormalizeVersion(packaging_version.Version):
-    def __init__(self, version):
-        self._orig_version = version
-        super().__init__(version)
+# build tf1's wheel file
+res = subprocess.run(["python3.7", "setup_tf1.py", "bdist_wheel"], shell=False)
+if res.returncode:
+    raise RuntimeError(f"build tf1's wheel file failed!")
 
-    def __str__(self):
-        return self._orig_version
+# build tf2's wheel file
+res = subprocess.run(["python3.7", "setup_tf2.py", "bdist_wheel"], shell=False)
+if res.returncode:
+    raise RuntimeError(f"build tf2's wheel file failed!")
 
-
-packaging_version.Version = NoNormalizeVersion
-# Patch safe_version() to prevent version normalization
-pkg_resources.safe_version = lambda v: v
-
-try:
-    with open("README.md") as file:
-        LONG_DESCRIPTION = file.read()
-except IOError:
-    LONG_DESCRIPTION = ""
-
-env_version = os.getenv("VERSION")
-VERSION = env_version if env_version is not None else '5.0.rc3'
-
-INIT_FILE = "mx_rec/__init__.py"
-with open(INIT_FILE, 'r') as file:
-    lines = file.readlines()
-
-for idx, line in enumerate(lines):
-    if "__version__ = " not in line:
-        continue
-    lines[idx] = f"__version__ = '{VERSION}'\n"
-    break
-
-FLAG = os.O_WRONLY | os.O_TRUNC
-MODE = stat.S_IWUSR | stat.S_IRUSR | stat.S_IRGRP | stat.S_IROTH
-with os.fdopen(os.open(INIT_FILE, FLAG, MODE), 'w') as out:
-    out.writelines(lines)
-
-setup(
-    name='mx_rec',
-    version=VERSION,
-    author='HUAWEI Inc',
-    description='MindX SDK Recommend',
-    long_description=LONG_DESCRIPTION,
-    # include mx_rec
-    packages=find_packages(
-        where='.',
-        include=["mx_rec*"]
-    ),
-    package_dir={},
-    # other file
-    package_data={'': ['tools/*', 'tools/*/*', '*.yml', '*.sh', '*.so*']},
-    # dependency
-    python_requires='>=3.7.5'
-)
+# copy cust_op, examples files, etc. Then gen mxrec's tar pkg
+res = subprocess.run(["bash", "./build/gen_mxrec_tar_pkg.sh"], shell=False)
+if res.returncode:
+    raise RuntimeError(f"gen mxrec's tar pkg failed!")
