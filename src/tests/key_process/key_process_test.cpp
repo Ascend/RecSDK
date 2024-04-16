93a734aa96f6d3499f5d8453ebd57a813c3d47bc
wuhongfa <1660398197@qq.com>
2024-04-16 21:05:41 +0800

    删除所有判断Hot embed的代码，默认开启

--- src/tests/key_process/key_process_test.cpp
+++ src/tests/key_process/key_process_test.cpp
@@ -321,6 +321,7 @@ protected:
     void TearDown()
     {
         // delete
+        GlobalMockObject::reset();
     }
 };
 
@@ -664,7 +665,7 @@ TEST_F(KeyProcessTest, KeyProcessTaskHelper)
     infoVecs->pop_back();
     int64_t hotPosition = process.hotEmbTotCount[batch->name];
     vector<int64_t> expectRestore(allExpectRestore[worldRank].size());
-    for(int i = 0; i < expectRestore.size(); i++) {
+    for (int i = 0; i < expectRestore.size(); i++) {
         expectRestore[i] = allExpectRestore[worldRank][i] + hotPosition;
     }
     ASSERT_EQ(CheckFlatTensor(*infoVecs, expectRestore), true);
