9239843a278b5d34bf8f457f259239b7feae6b75
wuhongfa <1660398197@qq.com>
2024-04-17 12:53:29 +0800

    删除所有判断Hot embed的代码，默认开启

--- mx_rec/core/asc/manager.py
+++ mx_rec/core/asc/manager.py
@@ -201,7 +201,7 @@ def initialize_emb_cache(table_info_list, threshold_list):
     if ConfigInitializer.get_instance().use_static:
         option = option | USE_STATIC
     # use hot always True
-    option = option | USE_STATIC << 1
+    # option = option | USE_STATIC << 1
     if ConfigInitializer.get_instance().use_dynamic_expansion:
         option = option | USE_DYNAMIC_EXPANSION
 
