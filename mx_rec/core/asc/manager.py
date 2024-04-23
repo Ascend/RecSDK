32685509402c9cf3a2fffc3a5c762c146788fe1d
罗幸运 <luoqianke@126.com>
2024-04-23 16:08:27 +0800

    【修改说明 Modification】根据优化器类型自动判断是否开启全局去重特性

--- mx_rec/core/asc/manager.py
+++ mx_rec/core/asc/manager.py
@@ -204,7 +204,7 @@ def initialize_emb_cache(table_info_list, threshold_list):
         option = option | USE_DYNAMIC_EXPANSION
 
     optimizer = ConfigInitializer.get_instance().optimizer_config.optimizer_instance
-    if optimizer.derivative == 2:
+    if optimizer and optimizer.derivative == 2:
         option = option | USE_SUM_SAME_ID_GRADIENTS
 
     # [train_steps, eval_steps, save_steps] pass step information to HybridMgmt for data process loop
