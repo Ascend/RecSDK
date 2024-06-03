84c5b10be2fffba7dc88908523a42b8c752e3854
郭望 <1244372993@qq.com>
2024-06-03 16:42:26 +0800

    WideDeep模型 迁移开源项目代码

--- examples/WideDeep/model/optimizer.py
+++ examples/WideDeep/model/optimizer.py
@@ -15,32 +15,20 @@
 # ==============================================================================
 
 import tensorflow as tf
-
 from delay_loss_scale import DenseLossScaleOptimizer, SparseLossScaleOptimizer
-from gradient_descent_w import create_hash_optimizer
 from mx_rec.util.initialize import ConfigInitializer
-from mx_rec.optimizers.gradient_descent_by_addr import create_hash_optimizer_by_addr
-from mx_rec.optimizers import lazy_adam
+from mx_rec.optimizers.lazy_adam import create_hash_optimizer
+from mx_rec.optimizers.lazy_adam_by_addr import create_hash_optimizer_by_address
 
 
 def get_dense_and_sparse_optimizer(cfg):
+    dense_optimizer = tf.train.AdamOptimizer(learning_rate=cfg.learning_rate[0])
     use_dynamic_expansion = ConfigInitializer.get_instance().use_dynamic_expansion
-    if cfg.use_lazy_adam_optimizer:
-        if use_dynamic_expansion:
-            raise RuntimeError("model is incompatible with dynamic_expansion when use lazy_adam optimizer.")
-        # use lazy_adam optimizer
-        dense_optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=cfg.learning_rate[0])
-        sparse_optimizer = lazy_adam.create_hash_optimizer(learning_rate=cfg.learning_rate[1])
-        loss_scale = 65536
+    if use_dynamic_expansion:
+        sparse_optimizer = create_hash_optimizer_by_address(learning_rate=cfg.learning_rate[1])
     else:
-        # use SGD optimizer
-        dense_optimizer = tf.compat.v1.train.GradientDescentOptimizer(learning_rate=cfg.learning_rate[0])
-        if use_dynamic_expansion:
-            sparse_optimizer = create_hash_optimizer_by_addr(learning_rate=cfg.learning_rate[1], weight_decay=0.0001)
-        else:
-            sparse_optimizer = create_hash_optimizer(learning_rate=cfg.learning_rate[1], weight_decay=0.0001)
-        loss_scale = 1024
-    sparse_optimizer = SparseLossScaleOptimizer(sparse_optimizer, loss_scale)
-    dense_optimizer = DenseLossScaleOptimizer(dense_optimizer, loss_scale)
+        sparse_optimizer = create_hash_optimizer(learning_rate=cfg.learning_rate[1])
+    sparse_optimizer = SparseLossScaleOptimizer(sparse_optimizer, 1)
+    dense_optimizer = DenseLossScaleOptimizer(dense_optimizer, 1)
 
     return dense_optimizer, sparse_optimizer
