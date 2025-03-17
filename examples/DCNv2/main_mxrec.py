11b79e445c9217ca2e81e68908baa869314162ec
rome_zhouyang <zhouyang153@huawei.com>
2025-03-17 16:38:58 +0800

    fix

--- examples/DCNv2/main_mxrec.py
+++ examples/DCNv2/main_mxrec.py
@@ -284,17 +284,13 @@ if __name__ == "__main__":
         use_multi_lookup = bool(int(os.getenv("USE_MULTI_LOOKUP", 0)))
         MODIFY_GRAPH_FLAG = bool(int(os.getenv("USE_MODIFY_GRAPH", 0)))
         use_faae = bool(int(os.getenv("USE_FAAE", 0)))
+        use_shm_swap = bool(int(os.getenv("USE_SHM_SWAP", 0)))
     except ValueError as err:
         raise ValueError("please correctly config USE_DYNAMIC_EXPANSION or USE_MULTI_LOOKUP or USE_FAAE "
                          "or USE_MODIFY_GRAPH only 0 or 1 is supported.") from err
 
     use_dynamic = bool(int(os.getenv("USE_DYNAMIC", 0)))
     use_lccl = bool(int(os.getenv("USE_LCCL", 0)))
-    shm_swap_value = os.getenv("USE_SHM_SWAP", 0)
-    if shm_swap_value not in [0, 1]:
-        shm_swap_value = 0
-        logger.info(f"USE_SHM_SWAP: {shm_swap_value} is illegal, use defualt")
-    use_shm_swap = bool(int(shm_swap_value))
     logger.info(f"USE_DYNAMIC: {use_dynamic}")
     init(train_steps=train_steps, eval_steps=eval_steps,
          use_dynamic=use_dynamic, use_dynamic_expansion=use_dynamic_expansion,
