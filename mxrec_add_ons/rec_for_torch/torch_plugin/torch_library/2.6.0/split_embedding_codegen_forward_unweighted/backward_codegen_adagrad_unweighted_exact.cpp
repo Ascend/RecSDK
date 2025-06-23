be081f22787263a1e16403787434a628a4c59f2b
wuyangjian <wuyangjian1115@163.com>
2025-06-23 15:31:05 +0800

    lookup开发，问题修复

--- mxrec_add_ons/rec_for_torch/torch_plugin/torch_library/2.6.0/split_embedding_codegen_forward_unweighted/backward_codegen_adagrad_unweighted_exact.cpp
+++ mxrec_add_ons/rec_for_torch/torch_plugin/torch_library/2.6.0/split_embedding_codegen_forward_unweighted/backward_codegen_adagrad_unweighted_exact.cpp
@@ -267,8 +267,8 @@ at::Tensor split_embedding_backward_codegen_adagrad_unweighted_exact_npu(
     double beta = 0;
     int64_t iter = 0;
     const auto grad_output_conti = grad_output.contiguous();
-    EXEC_NPU_CMD(aclnnBackwardCodegenAdagradUnweightedExact, grad_output_conti, dev_weights, uvm_weights, lxu_cache_weights,
-                 weights_placements, weights_offsets, D_offsets, hash_size_cumsum, indices, offsets,
+    EXEC_NPU_CMD(aclnnBackwardCodegenAdagradUnweightedExact, grad_output_conti, dev_weights, uvm_weights, 
+                 lxu_cache_weights, weights_placements, weights_offsets, D_offsets, hash_size_cumsum, indices, offsets,
                  lxu_cache_locations, momentum1_dev, momentum1_uvm, momentum1_placements, momentum1_offsets, _unused,
                  _unused, _unused, _unused, hash_indices, unique_ids, unique_offsets, unique_inverse, t_max_D,
                  total_hash_size_bits, pooling_mode, BT_block_size, max_segment_length_per_warp, stochastic_rounding,
--- mxrec_add_ons/rec_for_torch/torch_plugin/torch_library/2.6.0/split_embedding_codegen_forward_unweighted/backward_codegen_sgd_unweighted_exact.cpp
+++ mxrec_add_ons/rec_for_torch/torch_plugin/torch_library/2.6.0/split_embedding_codegen_forward_unweighted/backward_codegen_sgd_unweighted_exact.cpp
@@ -308,8 +308,8 @@ at::Tensor split_embedding_backward_codegen_sgd_unweighted_exact_npu(const Tenso
     const int iter = 0;
     const float beta = 0;
     const auto grad_output_conti = grad_output.contiguous();
-    EXEC_NPU_CMD(aclnnBackwardCodegenAdagradUnweightedExact, grad_output_conti, dev_weights, uvm_weights, lxu_cache_weights,
-                 weights_placements, weights_offsets, D_offsets, hash_size_cumsum, indices, offsets,
+    EXEC_NPU_CMD(aclnnBackwardCodegenAdagradUnweightedExact, grad_output_conti, dev_weights, uvm_weights, 
+                 lxu_cache_weights, weights_placements, weights_offsets, D_offsets, hash_size_cumsum, indices, offsets,
                  lxu_cache_locations, _unused, _unused, _unused, _unused, _unused, _unused, _unused, _unused,
                  hash_indices, unique_ids, unique_offsets, unique_inverse, t_max_D, total_hash_size_bits, pooling_mode,
                  BT_block_size, max_segment_length_per_warp, stochastic_rounding, info_B_num_bits, info_B_mask_int64,
