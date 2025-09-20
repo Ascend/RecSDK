1df73a43bbfe5da36db1efd6ae04affb0e007dfc
B1anrK <bianrkpt@163.com>
2025-09-20 12:58:08 +0800

    update: 更新文件 lccl_all_uss.cpp
    Signed-off-by: B1anrK <bianrkpt@163.com>

--- cust_op/ascendc_op/ai_core_op/lccl/v220/op_host/lccl_all_uss.cpp
+++ cust_op/ascendc_op/ai_core_op/lccl/v220/op_host/lccl_all_uss.cpp
@@ -37,6 +37,11 @@ static ge::graphStatus TilingFunc(gert::TilingContext* context)
     int rankSize = static_cast<int>(*rank_Size_);
 
     const gert::StorageShape* rev_shape = context->GetInputShape(4);  // Get embedding row num.
+    
+    OPS_CHECK(rankSize <= 0,
+        OPS_LOG_E("[ERROR]", "rankSize cannot be smaller than 1"), return ge::GRAPH_FAILED);
+    OPS_CHECK(rankSize > LCAL_MAX_RANK_SIZE,
+        OPS_LOG_E("[ERROR]", "rankSize exceeds maximum supported rank size"), return ge::GRAPH_FAILED);
 
     tiling.set_rank(rank);
     tiling.set_dim(dim);
