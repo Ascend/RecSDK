0b5201b97f8b4a66c6e0558fa800c1200945324f
B1anrK <bianrkpt@163.com>
2025-09-20 16:10:19 +0800

    update: 更新文件 lccl_gather_all.cpp
    Signed-off-by: B1anrK <bianrkpt@163.com>

--- cust_op/ascendc_op/ai_core_op/lccl/v220/op_host/lccl_gather_all.cpp
+++ cust_op/ascendc_op/ai_core_op/lccl/v220/op_host/lccl_gather_all.cpp
@@ -41,7 +41,9 @@ namespace optiling {
         const gert::StorageShape* rev_shape = context->GetInputShape(4); // get emb row num
 
         OPS_CHECK(rankSize <= 0 || rankSize > LCAL_MAX_RANK_SIZE,
-            OPS_LOG_E("[ERROR]", "rankSize is invalid: It must be at least 1 and must not exceed the maximum supported rank size."), 
+            OPS_LOG_E("[ERROR]", "Invalid rankSize: %d\n"
+            "Valid range: must be at least 1 and not exceed %d (maximum supported rank size).",
+            rankSize, LCAL_MAX_RANK_SIZE),
             return ge::GRAPH_FAILED);
 
         tiling.set_rank(rank);
