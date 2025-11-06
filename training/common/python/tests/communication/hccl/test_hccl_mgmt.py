f95e8e8f070f56a7e239b7af581c7328aca53109
B1anrK <bianrkpt@163.com>
2025-11-06 20:00:36 +0800

    [feat]ranktable适配
    Co-authored-by: B1anrK<bianrkpt@163.com>

--- training/common/python/tests/communication/hccl/test_hccl_mgmt.py
+++ training/common/python/tests/communication/hccl/test_hccl_mgmt.py
@@ -41,7 +41,11 @@ class HCCLMGMTTest(unittest.TestCase):
         """
         os.environ[RankTableInfo.RANK_TABLE_FILE.value] = self.rank_table_file
 
-    def test_get_rank_info_with_ranktable_when_attribute_error(self):
+    @mock.patch(
+        "rec_sdk_common.communication.hccl.hccl_mgmt._determine_ranktable_format",
+        return_value=False
+    )
+    def test_get_rank_info_with_ranktable_when_attribute_error(self, mock_determine):
         with patch(
             "builtins.open",
             mock_open(
@@ -72,8 +76,12 @@ class HCCLMGMTTest(unittest.TestCase):
         ) as mock_file:
             with self.assertRaises(AttributeError):
                 rank_to_device_dict, local_rank_size = _get_rank_info_with_ranktable()
-
-    def test_get_rank_info_with_ranktable_when_value_error(self):
+                
+    @mock.patch(
+        "rec_sdk_common.communication.hccl.hccl_mgmt._determine_ranktable_format",
+        return_value=False
+    )
+    def test_get_rank_info_with_ranktable_when_value_error(self, mock_determine):
         with patch(
             "builtins.open",
             mock_open(
