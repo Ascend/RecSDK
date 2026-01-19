38459f19df9f437cd9ee42b6098818dcbb2a7c52
tqy1998 <taoqiuyu2@huawei.com>
2026-01-19 23:00:06 +0800

    GPU适配
    Co-authored-by: taoqiuyu<taoqiuyu2@huawei.com>

---
+++ rec_model_zoo_pytorch/GR/GPU/GR_SparseMOE_Small/launch.sh
@@ -0,0 +1,24 @@
+# 拷贝代码
+git clone https://github.com/NVIDIA/recsys-examples.git
+cd recsys-examples && git checkout 0626593
+# 打补丁
+cp ../../GR_Base/GPU_GR.patch ./
+dos2unix  GPU_GR.patch  #   转为linux格式，打patch报错执行这个
+sed -i 's/[ \t]*$//' GPU_GR.patch
+git apply GPU_GR.patch
+echo "基础补丁加载成功"
+cp ../../GR_SparseMOE/GPU_GR_SparseMOE.patch ./
+cp ../../GR_SparseMOE/rab_module.patch ./
+dos2unix  GPU_GR_SparseMOE.patch  #   转为linux格式，打patch报错执行这个
+dos2unix  rab_module.patch  
+git apply GPU_GR_SparseMOE.patch
+git apply rab_module.patch
+
+cp ../GR_GPU.patch ./
+dos2unix  GR_GPU.patch
+git apply GR_GPU.patch
+
+echo "补丁加载成功，可进入recsys-examples/hstu目录,执行run_random_2k_xx.sh脚本"
+
+cd examples/hstu
+mkdir input_output
