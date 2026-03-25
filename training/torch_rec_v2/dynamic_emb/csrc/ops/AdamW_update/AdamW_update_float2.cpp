ec64dde5080e074105d5d841d1b6f04ea328d21a
Ronald_Z <zhurz1997@qq.com>
2026-03-25 16:50:22 +0800

    [fix][pt_v2][A5] unique, Adam, AdamW算子codecheck问题修复
    Co-authored-by: Ronald<zhurz1997@qq.com>

--- training/torch_rec_v2/dynamic_emb/csrc/ops/AdamW_update/AdamW_update_float2.cpp
+++ training/torch_rec_v2/dynamic_emb/csrc/ops/AdamW_update/AdamW_update_float2.cpp
@@ -19,7 +19,7 @@ See the License for the specific language governing permissions and
 
 constexpr int32_t BLOCK_THREADS = AdamWUpdateFloat2Simt::MAX_THREADS_PER_BLOCK;
 
-extern "C" __global__ __aicore__ void AdamW_update_float2(GM_ADDR grads, GM_ADDR values, int32_t gradDim,
+extern "C" __global__ __aicore__ void AdamW_update_float2(GM_ADDR grads, GM_ADDR values, uint32_t gradDim,
     int32_t inVecLength, float beta1, float beta2, float oneMinusBeta1, float oneMinusBeta2, float stepSize,
     float invVHatDenom, float decayFactor, float eps, int32_t totalBlocks, int32_t blocksPerCore,
     int32_t remainderBlocks, bool isSmall)
@@ -32,14 +32,14 @@ extern "C" __global__ __aicore__ void AdamW_update_float2(GM_ADDR grads, GM_ADDR
     __gm__ float2* __gm__* valuesPtr = reinterpret_cast<__gm__ float2* __gm__*>(values);
 
     // float2 下的 gradDim 步长，右移一位，等价于除以 2
-    int32_t gradDimVec = gradDim >> 1;
+    uint32_t gradDimVec = gradDim >> 1;
 
     // 判断 gradDimVec 是否为 2 的幂，便于后续逻辑分发
     bool isPowerOfTwo = (gradDimVec & (gradDimVec - 1)) == 0;
     int32_t gradDimVecShift = 0;
     if (isPowerOfTwo) {
-        int32_t gradDimCopy = gradDimVec;
-        while (gradDimCopy >>= 1) {
+        uint32_t gradDimCopy = gradDimVec;
+        while ((gradDimCopy >>= 1) != 0) {
             gradDimVecShift++;
         }
     }
