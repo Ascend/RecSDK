f95aa2afa1e5aa9787a6fc6c2b720542af3b0a6e
CUIZIBO <1565034584@qq.com>
2026-06-04 19:44:53 +0800

    [feat][A5][pt_v2]adamw精度修正
    Co-authored-by: CUIZIBO<1565034584@qq.com>

--- training/torch_rec_v2/dynamic_emb/csrc/ops/AdamW_update/AdamW_update_kernel.h
+++ training/torch_rec_v2/dynamic_emb/csrc/ops/AdamW_update/AdamW_update_kernel.h
@@ -20,28 +20,20 @@ See the License for the specific language governing permissions and
 using namespace AscendC;
 
 struct AdamWOptimizer {
-    static constexpr int32_t MAX_THREADS_PER_BLOCK  = 1024;
+    static constexpr int32_t MAX_THREADS_PER_BLOCK = 1024;
     static constexpr int32_t MAX_ELEMENTS_PER_THREAD = 4;
-    __simt_callee__  inline void updatefloat2(__gm__ float2* valuesRowBasePtr,
-        int32_t colVecIdx,
-        uint32_t gradDim,
-        float2 grad,
-        float beta1,
-        float beta2,
-        float oneMinusBeta1,
-        float oneMinusBeta2,
-        float stepSize,
-        float invVHatDenom,
-        float decayFactor,
-        float eps) const
+    __simt_callee__ inline void updatefloat2(__gm__ float2* valuesRowBasePtr, int32_t colVecIdx, uint32_t gradDim,
+                                             float2 grad, float beta1, float beta2, float oneMinusBeta1,
+                                             float oneMinusBeta2, float stepSize, float invVHatDenom, float decayFactor,
+                                             float eps) const
     {
         int32_t weightIdx = colVecIdx;
         int32_t mIdx = weightIdx + static_cast<int32_t>(gradDim);
         int32_t vIdx = mIdx + static_cast<int32_t>(gradDim);
 
         float2 tmpWeight = valuesRowBasePtr[weightIdx];
-        float2 tmpM      = valuesRowBasePtr[mIdx];
-        float2 tmpV      = valuesRowBasePtr[vIdx];
+        float2 tmpM = valuesRowBasePtr[mIdx];
+        float2 tmpV = valuesRowBasePtr[vIdx];
 
         float2 newM, newV, vHat, res_w;
 
@@ -55,28 +47,17 @@ struct AdamWOptimizer {
         vHat.x = newV.x * invVHatDenom;
         vHat.y = newV.y * invVHatDenom;
 
-        res_w.x = tmpWeight.x * decayFactor
-        - stepSize * newM.x / (AscendC::Simt::Sqrt(vHat.x + eps) );
-        res_w.y = tmpWeight.y * decayFactor
-        - stepSize * newM.y / (AscendC::Simt::Sqrt(vHat.y + eps) );
+        res_w.x = tmpWeight.x * decayFactor - stepSize * newM.x / (AscendC::Simt::Sqrt(vHat.x) + eps);
+        res_w.y = tmpWeight.y * decayFactor - stepSize * newM.y / (AscendC::Simt::Sqrt(vHat.y) + eps);
 
         valuesRowBasePtr[weightIdx] = res_w;
-        valuesRowBasePtr[mIdx]      = newM;
-        valuesRowBasePtr[vIdx]      = newV;     
+        valuesRowBasePtr[mIdx] = newM;
+        valuesRowBasePtr[vIdx] = newV;
     }
     template <typename grad_t, typename weight_t>
-    __simt_callee__  inline void update(__gm__ weight_t* valuesRowBasePtr,
-                                  int32_t colIdx,
-                                  uint32_t gradDim,
-                                  grad_t grad,
-                                  float beta1,
-                                  float beta2,
-                                  float oneMinusBeta1,
-                                  float oneMinusBeta2,
-                                  float stepSize,
-                                  float invVHatDenom,
-                                  float decayFactor,
-                                  float eps) const
+    __simt_callee__ inline void update(__gm__ weight_t* valuesRowBasePtr, int32_t colIdx, uint32_t gradDim, grad_t grad,
+                                       float beta1, float beta2, float oneMinusBeta1, float oneMinusBeta2,
+                                       float stepSize, float invVHatDenom, float decayFactor, float eps) const
     {
         int32_t weightIdx = colIdx;
         int32_t mIdx = weightIdx + static_cast<int32_t>(gradDim);
@@ -90,11 +71,10 @@ struct AdamWOptimizer {
         grad_t newV = beta2 * tmpV + oneMinusBeta2 * grad * grad;
 
         float vHat = newV * invVHatDenom;
-        float resW = tmpWeight * decayFactor
-                    - stepSize * newM / (AscendC::Simt::Sqrt(vHat + eps) );
+        float resW = tmpWeight * decayFactor - stepSize * newM / (AscendC::Simt::Sqrt(vHat) + eps);
 
         valuesRowBasePtr[weightIdx] = resW;
-        valuesRowBasePtr[mIdx]      = newM;
-        valuesRowBasePtr[vIdx]      = newV;
+        valuesRowBasePtr[mIdx] = newM;
+        valuesRowBasePtr[vIdx] = newV;
     }
 };
--- training/torch_rec_v2/dynamic_emb/tests/ut/dynamic_emb_op/test_AdamW_optimizer.py
+++ training/torch_rec_v2/dynamic_emb/tests/ut/dynamic_emb_op/test_AdamW_optimizer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+# Copyright (c) Huawei Technologies Co., Ltd. 2026. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""SGD / AdamW dynamic embedding optimizer update tests."""
+"""AdamW dynamic embedding optimizer update tests."""
 
 import pytest
 import torch
@@ -26,26 +26,7 @@ from optimizer_update_test_utils import (
     run_adamw_pointer_hybrid_test,
     run_adamw_pointer_test,
     run_adamw_table_test,
-    run_sgd_fused_hybrid_test,
-    run_sgd_fused_test,
-    run_sgd_pointer_hybrid_test,
-    run_sgd_pointer_test,
-    run_sgd_table_test,
 )
-
-# ---------------------------------------------------------------------------
-# SGD parametrization
-# ---------------------------------------------------------------------------
-_SGD_POINTER_BATCH = [1, 1024, 4096, 8192, 10240, 102400]
-_SGD_POINTER_DIM = [8, 64, 128, 256, 512, 1024, 31, 1023]
-_SGD_TABLE_BATCH = [1, 1024, 4096, 102400]
-_SGD_TABLE_DIM = [64, 128, 31, 1023]
-_SGD_FUSED_BATCH = [1, 1024, 4096, 8192]
-_SGD_FUSED_DIM = [8, 64, 128, 256, 31, 1023]
-_SGD_LR = [0.001, 0.01, 0.1]
-_SGD_LR_TABLE = [0.001, 0.01]
-_SGD_ITER = [10, 100]
-_SGD_FP32 = [torch.float32]
 
 # ---------------------------------------------------------------------------
 # AdamW parametrization
@@ -56,12 +37,13 @@ _ADAMW_TABLE_BATCH = [1, 1024, 4096, 102400]
 _ADAMW_TABLE_DIM = [64, 128, 31, 1023]
 _ADAMW_FUSED_BATCH = [1, 1024, 4096, 8192, 102400]
 _ADAMW_FUSED_DIM = [8, 64, 128, 256, 31, 1023]
-_ADAMW_HYBRID_FP32 = [(torch.float32, 1e-8)]
-_ADAMW_ALL_DTYPES = [
-    (torch.float32, 1e-8),
-    (torch.float16, 1e-4),
-    (torch.bfloat16, 1e-8),
+_ADAMW_EPS_FP32_BF16 = [1e-8, 0.1]
+_ADAMW_EPS_FP16 = [1e-4, 0.1]
+_ADAMW_GRAD_TYPE_EPS = [
+    *((grad_type, eps) for grad_type in (torch.float32, torch.bfloat16) for eps in _ADAMW_EPS_FP32_BF16),
+    *((grad_type, eps) for grad_type in (torch.float16,) for eps in _ADAMW_EPS_FP16),
 ]
+_ADAMW_HYBRID_GRAD_TYPE_EPS = [(torch.float32, eps) for eps in _ADAMW_EPS_FP32_BF16]
 
 
 @pytest.fixture(name="optimizer_params")
@@ -76,56 +58,6 @@ def _optimizer_params(lr, beta1, beta2, eps, weight_decay):
 
 
 @pytest.mark.parametrize("device", [0])
-@pytest.mark.parametrize("batch_size", _SGD_POINTER_BATCH)
-@pytest.mark.parametrize("embedding_dim", _SGD_POINTER_DIM)
-@pytest.mark.parametrize("lr", _SGD_LR)
-@pytest.mark.parametrize("iter_num", _SGD_ITER)
-@pytest.mark.parametrize("grad_type", _SGD_FP32)
-def test_dynamic_emb_sgd_with_pointer(device, batch_size, embedding_dim, lr, iter_num, grad_type):
-    run_sgd_pointer_test(device, batch_size, embedding_dim, lr, iter_num, grad_type)
-
-
-@pytest.mark.parametrize("device", [0])
-@pytest.mark.parametrize("batch_size", _SGD_TABLE_BATCH)
-@pytest.mark.parametrize("embedding_dim", _SGD_TABLE_DIM)
-@pytest.mark.parametrize("lr", _SGD_LR_TABLE)
-@pytest.mark.parametrize("iter_num", _SGD_ITER)
-@pytest.mark.parametrize("grad_type", _SGD_FP32)
-def test_dynamic_emb_sgd_with_table(device, batch_size, embedding_dim, lr, iter_num, grad_type):
-    run_sgd_table_test(device, batch_size, embedding_dim, lr, iter_num, grad_type)
-
-
-@pytest.mark.parametrize("device", [0])
-@pytest.mark.parametrize("batch_size", _SGD_FUSED_BATCH)
-@pytest.mark.parametrize("embedding_dim", _SGD_FUSED_DIM)
-@pytest.mark.parametrize("lr", _SGD_LR)
-@pytest.mark.parametrize("iter_num", _SGD_ITER)
-@pytest.mark.parametrize("grad_type", _SGD_FP32)
-def test_dynamic_emb_sgd_fused(device, batch_size, embedding_dim, lr, iter_num, grad_type):
-    run_sgd_fused_test(device, batch_size, embedding_dim, lr, iter_num, grad_type)
-
-
-@pytest.mark.parametrize("device", [0])
-@pytest.mark.parametrize("batch_size", _SGD_POINTER_BATCH)
-@pytest.mark.parametrize("embedding_dim", _SGD_POINTER_DIM)
-@pytest.mark.parametrize("lr", _SGD_LR)
-@pytest.mark.parametrize("iter_num", _SGD_ITER)
-@pytest.mark.parametrize("grad_type", _SGD_FP32)
-def test_dynamic_emb_sgd_with_pointer_hybrid(device, batch_size, embedding_dim, lr, iter_num, grad_type):
-    run_sgd_pointer_hybrid_test(device, batch_size, embedding_dim, lr, iter_num, grad_type)
-
-
-@pytest.mark.parametrize("device", [0])
-@pytest.mark.parametrize("batch_size", _SGD_FUSED_BATCH)
-@pytest.mark.parametrize("embedding_dim", _SGD_FUSED_DIM)
-@pytest.mark.parametrize("lr", _SGD_LR)
-@pytest.mark.parametrize("iter_num", _SGD_ITER)
-@pytest.mark.parametrize("grad_type", _SGD_FP32)
-def test_dynamic_emb_sgd_fused_hybrid(device, batch_size, embedding_dim, lr, iter_num, grad_type):
-    run_sgd_fused_hybrid_test(device, batch_size, embedding_dim, lr, iter_num, grad_type)
-
-
-@pytest.mark.parametrize("device", [0])
 @pytest.mark.parametrize("batch_size", _ADAMW_POINTER_BATCH)
 @pytest.mark.parametrize("embedding_dim", _ADAMW_POINTER_DIM)
 @pytest.mark.parametrize("lr", [0.001, 0.01, 0.1])
@@ -133,7 +65,7 @@ def test_dynamic_emb_sgd_fused_hybrid(device, batch_size, embedding_dim, lr, ite
 @pytest.mark.parametrize("beta2", [0.999])
 @pytest.mark.parametrize("weight_decay", [0.0, 0.001, 0.01, 0.1])
 @pytest.mark.parametrize("iter_num", [10, 100])
-@pytest.mark.parametrize(("grad_type", "eps"), _ADAMW_ALL_DTYPES)
+@pytest.mark.parametrize(("grad_type", "eps"), _ADAMW_GRAD_TYPE_EPS)
 def test_dynamic_emb_AdamW_with_pointer(device, batch_size, embedding_dim, optimizer_params, iter_num, grad_type):
     run_adamw_pointer_test(device, batch_size, embedding_dim, optimizer_params, iter_num, grad_type)
 
@@ -146,7 +78,7 @@ def test_dynamic_emb_AdamW_with_pointer(device, batch_size, embedding_dim, optim
 @pytest.mark.parametrize("beta2", [0.999])
 @pytest.mark.parametrize("weight_decay", [0.0, 0.01])
 @pytest.mark.parametrize("iter_num", [10, 100])
-@pytest.mark.parametrize(("grad_type", "eps"), _ADAMW_ALL_DTYPES)
+@pytest.mark.parametrize(("grad_type", "eps"), _ADAMW_GRAD_TYPE_EPS)
 def test_dynamic_emb_AdamW_with_table(device, batch_size, embedding_dim, optimizer_params, iter_num, grad_type):
     run_adamw_table_test(device, batch_size, embedding_dim, optimizer_params, iter_num, grad_type)
 
@@ -159,7 +91,7 @@ def test_dynamic_emb_AdamW_with_table(device, batch_size, embedding_dim, optimiz
 @pytest.mark.parametrize("beta2", [0.999])
 @pytest.mark.parametrize("weight_decay", [0.0, 0.01])
 @pytest.mark.parametrize("iter_num", [10, 100])
-@pytest.mark.parametrize(("grad_type", "eps"), _ADAMW_ALL_DTYPES)
+@pytest.mark.parametrize(("grad_type", "eps"), _ADAMW_GRAD_TYPE_EPS)
 def test_dynamic_emb_AdamW_fused(device, batch_size, embedding_dim, optimizer_params, iter_num, grad_type):
     run_adamw_fused_test(device, batch_size, embedding_dim, optimizer_params, iter_num, grad_type)
 
@@ -172,7 +104,7 @@ def test_dynamic_emb_AdamW_fused(device, batch_size, embedding_dim, optimizer_pa
 @pytest.mark.parametrize("beta2", [0.999])
 @pytest.mark.parametrize("weight_decay", [0.0, 0.001, 0.01, 0.1])
 @pytest.mark.parametrize("iter_num", [10, 100])
-@pytest.mark.parametrize(("grad_type", "eps"), _ADAMW_HYBRID_FP32)
+@pytest.mark.parametrize(("grad_type", "eps"), _ADAMW_HYBRID_GRAD_TYPE_EPS)
 def test_dynamic_emb_AdamW_with_pointer_hybrid(
     device, batch_size, embedding_dim, optimizer_params, iter_num, grad_type
 ):
@@ -187,6 +119,6 @@ def test_dynamic_emb_AdamW_with_pointer_hybrid(
 @pytest.mark.parametrize("beta2", [0.999])
 @pytest.mark.parametrize("weight_decay", [0.0, 0.01])
 @pytest.mark.parametrize("iter_num", [10, 100])
-@pytest.mark.parametrize(("grad_type", "eps"), _ADAMW_HYBRID_FP32)
+@pytest.mark.parametrize(("grad_type", "eps"), _ADAMW_HYBRID_GRAD_TYPE_EPS)
 def test_dynamic_emb_AdamW_fused_hybrid(device, batch_size, embedding_dim, optimizer_params, iter_num, grad_type):
     run_adamw_fused_hybrid_test(device, batch_size, embedding_dim, optimizer_params, iter_num, grad_type)
